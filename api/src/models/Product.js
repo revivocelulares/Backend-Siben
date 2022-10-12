932e35c4541ef8e6a732ec1ba02a28b7edafa7ea
Diego Ferreira <revivocelulares@gmail.com>
2022-10-12 00:12:22 -0300

    retoque

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -26,7 +26,7 @@ module.exports = (sequelize) => {
             allowNull: false,
         },
         description: {
-            type: DataTypes.TEXT(),
+            type: DataTypes.STRING(),
             allowNull: false,
         },
         price_usd: {
