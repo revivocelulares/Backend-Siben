e844e0658e003d75401c8662379adb8ac9b66c78
Diego Ferreira <revivocelulares@gmail.com>
2022-11-12 05:41:06 -0300

    Algo

--- api/src/app.js
+++ api/src/app.js
@@ -20,7 +20,7 @@ server.use((req, res, next) => {
   next();
 });
 
-server.use('/', routes);
+server.use('/api', routes);
 
 server.use((err, req, res, next) => {
     const status = err.status || 500;
