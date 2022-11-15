cd4e37eadf4c5e2a55bbc94985ff9426191a5ddf
Diego Ferreira <revivocelulares@gmail.com>
2022-11-15 05:24:05 -0300

    Faltan los get del product

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -4,7 +4,7 @@ const { addProduct, updateProduct, deleteProduct, hard_deleteProduct } = require
 
 route.post("/", verify_admin_token, addProduct);
 route.patch("/:id_product", verify_admin_token, updateProduct);
-route.patch("/:id_product", verify_admin_token, deleteProduct);
+route.patch("/delete/:id_product", verify_admin_token, deleteProduct);
 route.delete("/:id_product", verify_admin_token, hard_deleteProduct);
 
 module.exports = route;
