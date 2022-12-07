da6b745a8567af863ad25115752b231404db35df
Diego Ferreira <revivocelulares@gmail.com>
2022-12-07 20:51:35 -0300

    fixed file upload

--- api/src/app.js
+++ api/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const fileUpload = require('express-fileupload');
+const corss = require('corss');
 const routes = require('./routes/index.js');
 require('dotenv').config();
 require('./db.js');
@@ -14,6 +15,7 @@ server.use(express.json());
 server.use(cookieParser());
 server.use(fileUpload());
 server.use(morgan('dev'));
+server.use(corss());
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
