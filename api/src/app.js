1604f3ba3d2f85df2d3a0aef4e2309cbd694cfe4
Diego Ferreira <revivocelulares@gmail.com>
2022-12-30 22:03:13 -0300

    ADD MP route

--- api/src/app.js
+++ api/src/app.js
@@ -6,6 +6,7 @@ const corss = require('corss');
 const routes = require('./routes/index.js');
 require('dotenv').config();
 require('./db.js');
+const mercadopago = require('mercadopago');
 
 const server = express();
 
@@ -32,6 +33,38 @@ server.use((err, req, res, next) => {
     const message = err.message || err;
     console.error(err);
     res.status(status).send(message);
+});
+
+server.post("/process-payment", (req, res) => {
+  mercadopago.configurations.setAccessToken(process.env.ACCESS_TOKEN_MP);
+  const payment_data = {
+      transaction_amount: req.body.transaction_amount,
+      token: req.body.token,
+      description: req.body.description,
+      installments: Number(req.body.installments),
+      payment_method_id: req.body.paymentMethodId,
+      issuer_id: req.body.issuer,
+      payer: {
+          email: req.body.payer.email,
+          identification: {
+              type: req.body.payer.docType,
+              number: req.body.payer.docNumber,
+          },
+      },
+  };
+
+  mercadopago.payment
+      .save(payment_data)
+      .then((response) => {
+          return res.status(response.status).json({
+              id: response.body.id,
+              status: response.body.status,
+              status_detail: response.body.status_detail,
+          });
+      })
+      .catch((err) => {
+          return res.status(500).send(err);
+      });
 });
 
 module.exports = server;
