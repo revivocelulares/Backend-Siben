709dea1737e845b49c67782d8126c1ea37b2a265
Diego Ferreira <revivocelulares@gmail.com>
2023-01-06 23:05:45 -0300

    dolar

--- api/src/routes/dolar.js
+++ api/src/routes/dolar.js
@@ -6,8 +6,6 @@ route.get('/', async (req, res) => {
     {
         headers: {
             'Authorization': `Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDQ1MDg4ODIsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJyb21pbmFib2NvbkBob3RtYWlsLmNvbSJ9.G_b-fkqHKiL2gU2ncPwOfNI_BTJ46eidbueziqCrCtNMiXSvZskqM0btOqHSSKPyVuVHLcu4220H71YQS1geZg`,
-            "Access-Control-Allow-Origin": "*",
-            'Access-Control-Allow-Methods': 'GET',
         },
     });
     return res.send(dolar.data[dolar.data.length - 1]);
