8c94bba55dcd7053aabd4e4da8bf6a944e567581
Diego Ferreira <revivocelulares@gmail.com>
2023-01-18 00:52:00 -0300

    post

--- api/src/routes/paymentPP.js
+++ api/src/routes/paymentPP.js
@@ -4,6 +4,6 @@ const { createOrder, captureOrder, cancelOrder, sendResponse } = require('../con
 route.post('/paypal/create-order', createOrder);
 route.get('/paypal/capture-order', captureOrder);
 route.get('/paypal/cancel-order', cancelOrder);
-route.get('/paypal/send-response', sendResponse)
+route.post('/paypal/send-response', sendResponse)
 
 module.exports = route;
