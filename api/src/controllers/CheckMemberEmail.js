b0939fc3221bfb8c073f2e7b47db6e7cfdb54df3
Diego Ferreira <revivocelulares@gmail.com>
2023-02-14 17:11:34 -0300

    Fix connect MySQL-SIBEN

--- api/src/controllers/CheckMemberEmail.js
+++ api/src/controllers/CheckMemberEmail.js
@@ -1,35 +1,24 @@
-const mysql = require('mysql2');
+const { dbconn } = require('../configDBsiben');
 require('dotenv').config();
-const { SIBEN_DB_USER, SIBEN_DB_PASSWORD, SIBEN_DB_HOST, SIBEN_DB_NAME } = process.env;
 
 const check = {
     checkSibenEmail: async (req, res) => {
-        const conectar = mysql.createConnection({
-            host: SIBEN_DB_HOST,
-            user: SIBEN_DB_USER,
-            password: SIBEN_DB_PASSWORD,
-            database: SIBEN_DB_NAME
-        });
-
-        const conn = await conectar();
-
         const query = 'SELECT * FROM users WHERE group_id=3 AND active=1';
 
-        conn.connect((error) => {
-            if (error){
-                return console.log('Error: ' + error.message);
-            }
-            console.log('Conectado a la DB de SIBEN');
-        });
+        try {
+            const conn = await dbconn();          
 
-        conn.query(query, (error, results, fields) =>{
-            if(error) {
-                return console.error('Error: ' + error.message);
-            }
-            console.log(results[0]);
-            return res.status(200).json(results[0]);
-        });
-        conn.end;
+            conn.query(query, (error, results, fields) =>{
+                if(error) {
+                    return console.error('Error: ' + error.message);
+                }
+                console.log(results[0]);
+                return res.status(200).json(results[0]);
+            });
+            conn.end;
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 
