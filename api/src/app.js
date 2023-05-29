2c45b76357e68705c156831613dad78d50c8275c
Diego Ferreira <revivocelulares@gmail.com>
2023-05-29 13:21:12 -0300

    CORS

--- api/src/app.js
+++ api/src/app.js
@@ -20,7 +20,7 @@ server.use(corss());
 server.use('/api/tapa-libros', express.static(__dirname + '/tapa-libros'));
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'false');
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   next();
