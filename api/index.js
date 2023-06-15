42efc77e6774ed5fe5283bdc15ea770890ef20e3
Diego Ferreira <revivocelulares@gmail.com>
2023-06-15 20:40:00 -0300

    Hola

--- api/index.js
+++ api/index.js
@@ -3,10 +3,9 @@ const { conn } = require('./src/db.js');
 require('dotenv').config();
 
 //Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
     server.listen(process.env.PORT, () => {
       console.log('server listening on port 3001');
     });
   }, (error) => console.log(error));
 
-//Coment
